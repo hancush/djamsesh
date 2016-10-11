13dac4a1559b0489c549e52ffc3c3fd05caf0576
Hannah <hannah.cushman@gmail.com>
2016-10-11 16:10:22 -0500

    YOOOOOOOOOOO

--- blurgh/views.py
+++ blurgh/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-	posts = Post.objects
-	published_posts = posts.filter(published_date__lte=timezone.now()).order_by('published_date')
+	published_posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blurgh/post_list.html', {'published_posts': published_posts})
+
+def post_detail(request, post_id):
+	post = get_object_or_404(Post, pk=post_id)
+	return render(request, 'blurgh/post_detail.html', {'post': post})
